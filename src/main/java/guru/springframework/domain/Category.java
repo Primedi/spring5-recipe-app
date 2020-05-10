9b7b97632a98cabb7fec0ca8639b996d25c7690f
Thomas <ich@du>
2020-05-10 14:42:56 +0200

    Category fertiggestellt und ein DB-Init-Skript im Root-Verzeichnis angelegt (data.sql). Das wird standardmäßig ausgeführt

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -10,4 +10,29 @@ public class Category {
     private  Long id;
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT into CATEGORY (description) values ('german');
+INSERT into CATEGORY (description) values ('spanish');
+INSERT into CATEGORY (description) values ('british');
+INSERT into CATEGORY (description) values ('french');
+
+INSERT into unit_of_measure (description) values ('MSP');
+INSERT into unit_of_measure (description) values ('g');
+INSERT into unit_of_measure (description) values ('ml');
+INSERT into unit_of_measure (description) values ('TL');
+INSERT into unit_of_measure (description) values ('EL');
